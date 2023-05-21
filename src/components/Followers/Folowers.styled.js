1ac465ecededaa5ef47caf52343f8356cec1d477
Dmytrii Bevziuk <113050865+dburdin@users.noreply.github.com>
2023-05-21 22:38:08 +0300

    centring container

--- src/components/Followers/Folowers.styled.js
+++ src/components/Followers/Folowers.styled.js
@@ -3,6 +3,7 @@ import styled from "styled-components";
 export const Container = styled.div`
   width: fit-content;
   padding: 15px;
+  margin: 0 auto;
 
   display: flex;
   flex-direction: column;
